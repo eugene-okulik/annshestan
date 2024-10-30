1d2cc95fec1cb1fb843989bb23d43b15f8a50a53
Hanna Shastak <shastak@Shastak-MacBook-Pro.local>
2024-10-30 20:46:15 +0300

    Homework_3 fixed code standards issues

--- homework/anna_shestak/Homework_3/task_3.py
+++ homework/anna_shestak/Homework_3/task_3.py
@@ -3,7 +3,7 @@ import math
 x = 10
 y = 34
 
-arithm_average = (x+y)/2
-geom_average = math.sqrt(x*y)
+arithm_average = (x + y) / 2
+geom_average = math.sqrt(x * y)
 print(arithm_average)
 print(geom_average)
