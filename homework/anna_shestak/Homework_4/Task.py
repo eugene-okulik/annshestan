a499d7ecd10eff144fd691f61191c7996dae7c2d
Hanna Shastak <shastak@Shastak-MacBook-Pro.local>
2024-10-31 22:13:03 +0300

    Homework_4 done

---
+++ homework/anna_shestak/Homework_4/Task.py
@@ -0,0 +1,17 @@
+
+my_dict = {'tuple': (1, True, 6, 'Text'), 'list': [3, 'list', 5], 'dict': {'1': 'one', '2': 'two'}, 'set': {1, 3, 5}}
+ttuple = my_dict['tuple']
+print(ttuple[-1])
+llist = my_dict['list']
+llist.append('hehe')
+llist.pop(1)
+print(llist)
+ddict = my_dict['dict']
+ddict['i am tuple'] = 'three'
+ddict.pop('1')
+print(ddict)
+sset = my_dict['set']
+sset.add('not hehe')
+sset.pop()
+print(sset)
+print(my_dict)
