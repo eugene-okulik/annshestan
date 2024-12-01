7c27fc7aa62b94b0f02552073e0de6b5ec06eed8
Hanna Shastak <shastak@Shastak-MacBook-Pro.local>
2024-12-01 11:56:03 +0300

    style changes for final Task 2 fix

--- homework/anna_shestak/Homework_5/Task 2.py
+++ homework/anna_shestak/Homework_5/Task 2.py
@@ -7,9 +7,9 @@ result_1 = str_1.index(':')
 result_2 = str_2.index(':')
 result_3 = str_3.index(':')
 
-num_1 = int((str_1[(result_1+1)::]).lstrip())+10
-num_2 = int((str_2[(result_2+1)::]).lstrip())+10
-num_3 = int((str_3[(result_3+1)::]).lstrip())+10
+num_1 = int((str_1[(result_1+1)::]).lstrip()) + 10
+num_2 = int((str_2[(result_2+1)::]).lstrip()) + 10
+num_3 = int((str_3[(result_3+1)::]).lstrip()) + 10
 
 print(num_1)
 print(num_2)
