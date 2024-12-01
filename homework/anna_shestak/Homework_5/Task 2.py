245558c162c28a5105d85e964662de19656f5b56
Hanna Shastak <shastak@Shastak-MacBook-Pro.local>
2024-12-01 11:52:06 +0300

    Fixed Task 2 of Lesson 5  with generic solution

--- homework/anna_shestak/Homework_5/Task 2.py
+++ homework/anna_shestak/Homework_5/Task 2.py
@@ -1,15 +1,15 @@
 
 str_1 = 'результат операции: 42'
 str_2 = 'результат операции: 514'
-str_3 = 'результат операции: 9'
+str_3 = 'результат работы программы: 9'
 
-result_1 = str_1.index('42')
-result_2 = str_2.index('514')
-result_3 = str_3.index('9')
+result_1 = str_1.index(':')
+result_2 = str_2.index(':')
+result_3 = str_3.index(':')
 
-num_1 = int(str_1[result_1::]) + 10
-num_2 = int(str_2[result_2::]) + 10
-num_3 = int(str_3[result_3::]) + 10
+num_1 = int((str_1[(result_1+1)::]).lstrip())+10
+num_2 = int((str_2[(result_2+1)::]).lstrip())+10
+num_3 = int((str_3[(result_3+1)::]).lstrip())+10
 
 print(num_1)
 print(num_2)
