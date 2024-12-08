dabc787e66b5676a80b95596e481cb45697c403d
Hanna Shastak <shastak@Shastak-MBP.bbrouter>
2024-12-08 13:10:53 +0300

    Style changes for Task 2

--- homework/anna_shestak/Homework_7/Task_2.py
+++ homework/anna_shestak/Homework_7/Task_2.py
@@ -3,7 +3,7 @@ dictionary = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
 def copying(pairs):
     for word, quantity in pairs.items():
-        print(word*quantity)
+        print(word * quantity)
 
 
 copying(dictionary)
